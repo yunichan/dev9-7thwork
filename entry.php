adac198e7da132ac4e4708cbdad9cab7bd17a75e
yuni <yuni.tonoseaki@gmial.com>
2018-01-20 18:43:51 +0900

    test

--- entry.php
+++ entry.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>登録フォーム</title>
+    <title>登録</title>
     <link rel="stylesheet" href="css/sanitize.css"> 
     <link rel="stylesheet" href="css/style.css">
 </head>
